0d22cb0f02669690fc6d15115fdc6a9f3996f1c9
Kesava78615 <153480380+kesavajella@users.noreply.github.com>
2025-12-05 13:18:58 +0530

    Add input handling and calculation in Frames.c
    Implement basic input reading and calculation logic.

---
+++ C Programing/Frames.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,x;
+// Editable code starts here
+	//write your code to read the input
+	scanf("%d%d%d",&n,&m,&x);
+// Editable code ends here
+// Constrained code starts here
+	int sum=n*2+m*2;
+	x=sum*x;
+
+// Constrained code ends here
+	printf("%d
+",x);
+}
